350d916aa35a9dfa2cc3a0c61f90f00c9cd5af68
Dunnissimo <148977672+Dunnissimo@users.noreply.github.com>
2023-10-30 17:43:21 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,18 +1,18 @@
--- 
+-- customers_count.csv
 select count(customer_id) as customers_count
 from customers;
 -- Данный запрос находит общее количество покупателей из таблицы customers
 
-
+-- top_10_total_income.csv
 select 
-	 e.first_name || ' ' || e.last_name as name,
-	 count(s.sales_id) as operations,
-	 sum(p.price * s.quantity) as income
+	 e.first_name || ' ' || e.last_name as name, -- объединяем столбцы с именем и фамилией в один из таблици employees
+	 count(s.sales_id) as operations, -- считаем кол-во проведенных сделок за все время из таблицы 
+	 sum(p.price * s.quantity) as income -- перемножаем кол-во проданного товара и его цену, чтобы получить выручку и суммирем результат, чтобы узнать суммарную выручку продавца за все время
 from employees e
-left join sales s
+left join sales s -- объединяем таблицы employees и sales
 on e.employee_id = s.sales_person_id
-left join products p
+left join products p -- объединяем таблицы sales и products
 on s.product_id = p.product_id
-group by 1
-order by 3 desc nulls last
-limit 10;
+group by 1 -- группируем таблицу по имени продавца
+order by 3 desc nulls last -- сортируем по убыванию выручки, перенося вероятные нулы в конец таблицы
+limit 10; -- ограничиваем результат 10ю строками
